2e568c05da5ea0c578fdca0106b86e3f28962ab0
CarnifX <thecarnifx@gmail.com>
2024-10-22 11:13:31 +0200

    Gathering functions in their own file.

--- applicationWriter.py
+++ applicationWriter.py
@@ -1,43 +1,7 @@
-from bs4 import BeautifulSoup
 from dotenv import load_dotenv
-import requests
 import html2text
 from openai import OpenAI
-import PyPDF2
-import docx2txt
-
-
-#Funksjonen henter ut text fra PDF-filen, som i dette tilfellet inneholder en CV.
-def text_from_pdf(pdf_file):
-    with open(pdf_file, "rb") as f:
-        pdf = PyPDF2.PdfReader(f)
-        text = ""
-        for page in pdf.pages:
-            text = text + page.extract_text() + " "
-        return text
-
-
-#Enkel funksjon som returnerer en string med docx tekst.
-def text_from_docx(docx_file):
-    return docx2txt.process(docx_file)
-
-
-#Funksjonen web scraper tekst fra jobbannonse hentet fra
-#"import-decoration' i html koden til en finn.no link.
-#Det er her selve jobbeksrivelsen ligger
-def get_text_from_web_link(web_address):
-
-    page_to_scrape = requests.get(web_address)
-    soup = BeautifulSoup(page_to_scrape.text, "html.parser")
-    return str(soup.findAll("div", attrs={"class": "import-decoration"}))
-
-#Enkel funksjon som åpner en tekstfil lagret i repertoaret,
-#skriver inn stringen som kommer in som argument, og lukker filen
-def write_to_txt_file(text):
-    cover_letter_file = open("cover_letter.txt", "w")
-    cover_letter_file.write(text)
-    cover_letter_file.close()
-
+from utility_functions import*
 
 #Oppretter tre strings: en for CV(curriculum_vitae) fra pdf, en for CV(curriculum_vitae) fra docx,
 #og en for annonseteksten(text_content).
